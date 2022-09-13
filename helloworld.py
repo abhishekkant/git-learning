9a1b01f223fcd915af89ef5a2504a9d6a9e4f4f4
Abhishek Kant <abhishek@abhishekkant.net>
2022-09-13 17:11:07 +0530

    Adds Indian Welcome Message

--- helloworld.py
+++ helloworld.py
@@ -3,5 +3,8 @@ import greeting as g
 hello = g.greeting('Hello World - Elevators')
 hello.hello()
 
+indiahello = g.greeting("Namaste")
+indiahello.hello()
+
 hellonum = g.greeting(100)
 hellonum.hello()
