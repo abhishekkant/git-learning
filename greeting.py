1cb81d81bb27c5042490f7dff6733367b01e6145
Abhishek Kant <abhishek@abhishekkant.net>
2022-09-13 16:33:49 +0530

    Adds function to greet (hello)

--- greeting.py
+++ greeting.py
@@ -2,6 +2,13 @@ class greeting:
     def __init__(self, args):
         # Check if the args supplied is a string
         if (type(args) == str):
-            print(args)
+            self.message = args
         else:
             print("Please supply a string for greeting")
+
+
+    def hello(self):
+        try:
+            print(self.message)
+        except:
+            print("Something went wrong. Please contact the developer.")
--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
 import greeting as g
 
 hello = g.greeting('Hello World - Elevators')
+hello.hello()
 
 hellonum = g.greeting(100)
+hellonum.hello()
