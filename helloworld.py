fe2c62076eb41ddac6c18ab2494fbf88255dfabc
Utkarsh <guptaji8840@gmail.com>
2022-09-13 16:59:49 +0530

    adds greeting in french

--- helloworld.py
+++ helloworld.py
@@ -3,5 +3,9 @@ import greeting as g
 hello = g.greeting('Hello World - Elevators')
 hello.hello()
 
+bonjour = g.greeting('Hello in French!')
+bonjour.hello()
+
 hellonum = g.greeting(100)
 hellonum.hello()
+
