02f56d2d18506f2a7bbf0edd6343efb970de791e
Abhishek Kant <abhishek@abhishekkant.net>
2022-09-13 16:07:56 +0530

    Add check if supplied argument is a string

--- greeting.py
+++ greeting.py
@@ -1,3 +1,7 @@
 class greeting:
     def __init__(self, args):
-        print(args)
+        # Check if the args supplied is a string
+        if (type(args) == str):
+            print(args)
+        else:
+            print("Please supply a string for greeting")
--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
 import greeting as g
 
 hello = g.greeting('Hello World - Elevators')
+
+hellonum = g.greeting(100)
